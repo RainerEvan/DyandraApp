46d71f3d588a4a0f776cce93bb32598bce2962ba
RainerEvan <rainerevan@gmail.com>
2022-09-15 14:54:07 +0700

    first commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { WebdatarocksPivotModule } from 'ng-webdatarocks';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PivotTableComponent } from './components/pivot-table/pivot-table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PivotTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    WebdatarocksPivotModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/components/pivot-table/pivot-table.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PivotTableComponent } from './pivot-table.component';
+
+describe('PivotTableComponent', () => {
+  let component: PivotTableComponent;
+  let fixture: ComponentFixture<PivotTableComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PivotTableComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PivotTableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
