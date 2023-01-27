fc6a1ac4245c80b60a727fe63b532cba9b982543
RAINER EVAN <U545587@bca.co.id>
2023-01-27 13:54:15 +0700

    update app properties

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
-  // apiUrl:'http://10.43.6.167:3093/api',
+  // apiUrl: 'http://localhost:8080/api',
+  apiUrl:'http://10.43.6.167:3093/api',
 };
 
 /*
